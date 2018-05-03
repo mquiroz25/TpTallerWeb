ff38315f646414f38eeada72f55897845ebef042
damian-villarreal <damianvr@gmail.com>
2018-05-02 21:08:12 -0300

    agregadas clases barrio y punto

--- src/main/java/ar/edu/unlam/tallerweb1/modelo/Barrio.java
+++ src/main/java/ar/edu/unlam/tallerweb1/modelo/Barrio.java
@@ -1,8 +1,75 @@
 package ar.edu.unlam.tallerweb1.modelo;
 
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
 
 @Entity
 public class Barrio {
-
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String nombre;
+	
+	@ManyToOne (fetch = FetchType.LAZY)// LAZY and EAGER
+	@JoinColumn(name="idComuna")
+	private Comuna comuna;
+	
+	//constructores
+	
+	public Barrio(String nombre)
+	{this.nombre=nombre;}
+	
+	public Barrio()
+	{}
+	
+	//getters and setters
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public Comuna getComuna() {
+		return comuna;
+	}
+	public void setComuna(Comuna comuna) {
+		this.comuna = comuna;
+	}
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Barrio other = (Barrio) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
+	
 }
--- src/main/java/ar/edu/unlam/tallerweb1/modelo/Punto.java
+++ src/main/java/ar/edu/unlam/tallerweb1/modelo/Punto.java
@@ -1,8 +1,40 @@
 package ar.edu.unlam.tallerweb1.modelo;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
 @Entity
 public class Punto {
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String latitud;
+	private String longitud;
+	
+	
+	
+	
+	//getters and setters
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getLatitud() {
+		return latitud;
+	}
+	public void setLatitud(String latitud) {
+		this.latitud = latitud;
+	}
+	public String getLongitud() {
+		return longitud;
+	}
+	public void setLongitud(String longitud) {
+		this.longitud = longitud;
+	}
 }
+
